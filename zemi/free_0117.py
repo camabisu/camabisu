7346f14a8c3ac77e235d1ffebf5988d2d7c66d8f
camabisu <camembert0411@gmail.com>
2025-02-04 15:23:07 +0900

    Update free_0117.py

--- zemi/free_0117.py
+++ zemi/free_0117.py
@@ -123,7 +123,7 @@ class HappyMove(Node):
             while not self.move_distance(x):
                 rclpy.spin_once(self)
             self.x0, self.y0 = self.x, self.y  # 次の辺の開始位置を更新
-
+    def draw_rotate_angle(self,yaw):
             # 120度回転
             while not self.rotate_angle(math.pi*2 / 3):  # 2π/3ラジアン (120度) 回転
                 rclpy.spin_once(self)
